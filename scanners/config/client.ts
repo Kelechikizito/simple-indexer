040c64c26cd8aa6f247874c20bc6016b2264d13e
Kelechikizito <kaylaychi77@gmail.com>
2026-04-03 15:46:26 +0100

    update: multi-chain json-rpc

--- scanners/config/client.ts
+++ scanners/config/client.ts
@@ -1,7 +1,59 @@
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+import { arbitrum } from "viem/chains";
+import { optimism } from "viem/chains";
+import { base } from "viem/chains";
+import { zksync } from "viem/chains";
+import { avalanche } from "viem/chains";
+import { linea } from "viem/chains";
+import { monad } from "viem/chains";
 
 export const publicClient = createPublicClient({
   chain: mainnet,
-  transport: http("https://eth-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ")
-})
+  transport: http("https://eth-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+});
+
+export const publicClientArbitrum = createPublicClient({
+  chain: arbitrum,
+  transport: http("https://arb-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+});
+
+export const publicClientOptimism = createPublicClient({
+  chain: optimism,
+  transport: http("https://opt-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+});
+
+export const publicClientBase = createPublicClient({
+  chain: base,
+  transport: http(
+    "https://base-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
+  ),
+});
+
+export const publicClientZksync = createPublicClient({
+  chain: zksync,
+  transport: http(
+    "https://zksync-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
+  ),
+});
+
+export const publicClientAvalanche = createPublicClient({
+  chain: avalanche,
+  transport: http(
+    "https://avax-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
+  ),
+});
+
+export const publicClientLinea = createPublicClient({
+  chain: linea,
+  transport: http(
+    "https://linea-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
+  ),
+});
+
+export const publicClientMonad = createPublicClient({
+  chain: monad,
+  transport: http(
+    "https://monad-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
+  ),
+});
