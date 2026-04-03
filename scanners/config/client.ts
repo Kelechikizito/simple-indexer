f7f19fb55ded2e51b897cec6e38df22ef9a438e2
Kelechikizito <kaylaychi77@gmail.com>
2026-04-03 22:52:33 +0100

    updat: dotenv

--- scanners/config/client.ts
+++ scanners/config/client.ts
@@ -15,59 +15,57 @@ import {
   polygon,
 } from "viem/chains";
 
+const ETH_MAINNET_API_KEY = process.env.ETH_MAINNET_API_KEY;
+const ARBITRUM_MAINNET_API_KEY = process.env.ARBITRUM_MAINNET_API_KEY;
+const OPTIMISM_MAINNET_API_KEY = process.env.OPTIMISM_MAINNET_API_KEY;
+const BASE_MAINNET_API_KEY = process.env.BASE_MAINNET_API_KEY;
+const ZKSYNC_MAINNET_API_KEY = process.env.ZKSYNC_MAINNET_API_KEY;
+const AVALANCHE_MAINNET_API_KEY = process.env.AVALANCHE_MAINNET_API_KEY;
+const LINEA_MAINNET_API_KEY = process.env.LINEA_MAINNET_API_KEY;
+const MONAD_MAINNET_API_KEY = process.env.MONAD_MAINNET_API_KEY;
+const POLYGON_MAINNET_API_KEY = process.env.POLYGON_MAINNET_API_KEY;
+
 export const publicClient = createPublicClient({
   chain: mainnet,
-  transport: http("https://eth-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+  transport: http(`${ETH_MAINNET_API_KEY}`),
 });
 
 export const publicClientArbitrum = createPublicClient({
   chain: arbitrum,
-  transport: http("https://arb-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+  transport: http(`${ARBITRUM_MAINNET_API_KEY}`),
 });
 
 export const publicClientOptimism = createPublicClient({
   chain: optimism,
-  transport: http("https://opt-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ"),
+  transport: http(`${OPTIMISM_MAINNET_API_KEY}`),
 });
 
 export const publicClientBase = createPublicClient({
   chain: base,
-  transport: http(
-    "https://base-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${BASE_MAINNET_API_KEY}`),
 });
 
 export const publicClientZksync = createPublicClient({
   chain: zksync,
-  transport: http(
-    "https://zksync-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${ZKSYNC_MAINNET_API_KEY}`),
 });
 
 export const publicClientAvalanche = createPublicClient({
   chain: avalanche,
-  transport: http(
-    "https://avax-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${AVALANCHE_MAINNET_API_KEY}`),
 });
 
 export const publicClientLinea = createPublicClient({
   chain: linea,
-  transport: http(
-    "https://linea-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${LINEA_MAINNET_API_KEY}`),
 });
 
 export const publicClientMonad = createPublicClient({
   chain: monad,
-  transport: http(
-    "https://monad-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${MONAD_MAINNET_API_KEY}`),
 });
 
 export const publicClientPolygon = createPublicClient({
   chain: polygon,
-  transport: http(
-    "https://polygon-mainnet.g.alchemy.com/v2/17GHtHVS2qieiVAr5wJjZ",
-  ),
+  transport: http(`${POLYGON_MAINNET_API_KEY}`),
 });
