96f2a9ee4cc45087182a64a7d280cef6bddd83ec
Kelechikizito <kaylaychi77@gmail.com>
2026-04-15 07:26:41 +0100

    update: aave scanner

--- scanners/core/aave-scanner.ts
+++ scanners/core/aave-scanner.ts
@@ -18,8 +18,35 @@ import { aaveV3PoolAddresses } from "./aave-v3/pool-addresses.ts";
 import { aaveV3LiquidationEventAbi } from "./liquidation-event-abi.ts";
 
 let lastIndexedBlock: bigint | null = null;
-let lastIndexedBlockArb: bigint | null = null;
 
-const latestBlock = await latestBlockMainnet();
-const latestArbitrumBlock = await latestBlockArbitrum();
-const latestOptimismBlock = await latestBlockOptimism();
+async function getLiquidateEventLogs(fromBlock: bigint, toBlock: bigint) {
+  const mainnetLogs = await publicClient.getLogs({
+    address: aaveV3PoolAddresses[1],
+    event: aaveV3LiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return mainnetLogs;
+}
+
+while (true) {
+  const latestBlockMainnetNumber = await latestBlockMainnet();
+  const latestArbitrumBlock = await latestBlockArbitrum();
+
+  if (!latestBlockMainnetNumber) {
+    await new Promise((r) => setTimeout(r, 30_000));
+    continue;
+  }
+
+  const fromBlock = lastIndexedBlock
+    ? lastIndexedBlock + 1n
+    : latestBlockMainnetNumber - 5n;
+  const logs = await getLiquidateEventLogs(fromBlock, latestBlockMainnetNumber);
+  console.log(
+    `Fetched ${logs.length} logs from block ${fromBlock} to ${latestBlockMainnetNumber}`,
+  );
+
+  lastIndexedBlock = latestBlockMainnetNumber;
+  console.log(`Done. Sleeping 30 seconds...`);
+  await new Promise((r) => setTimeout(r, 30_000));
+}
