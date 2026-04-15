3d82363772c07374e59713a8491ac3dd9244e2c6
Kelechikizito <kaylaychi77@gmail.com>
2026-04-15 16:53:15 +0100

    update: morpho scanner

--- scanners/core/morpho-scanner.ts
+++ scanners/core/morpho-scanner.ts
@@ -41,3 +41,31 @@ async function getLiquidateEventMainnet(fromBlock: bigint, toBlock: bigint) {
   return mainnetLogs;
 }
 
+while (true) {
+  const latestBlockMainnetNumber = await latestBlockMainnet();
+
+  if (!latestBlockMainnetNumber) {
+    await new Promise((r) => setTimeout(r, 30_000));
+    continue;
+  }
+
+  const fromBlock = lastIndexedBlock
+    ? lastIndexedBlock + 1n
+    : latestBlockMainnetNumber - 5n;
+
+  const toBlock =
+    latestBlockMainnetNumber - fromBlock > 9n
+      ? fromBlock + 9n
+      : latestBlockMainnetNumber;
+
+  const logs = await getLiquidateEventMainnet(fromBlock, toBlock);
+
+  console.log(
+    `Fetched ${logs.length} logs from block ${fromBlock} to ${latestBlockMainnetNumber}`,
+  );
+
+  lastIndexedBlock = toBlock;
+
+  console.log(`Done. Sleeping 30 seconds...`);
+  await new Promise((r) => setTimeout(r, 30_000));
+}
