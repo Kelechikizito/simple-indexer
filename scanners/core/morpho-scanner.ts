eccb9a585c829473d826ed5846d09714e60b8574
Kelechikizito <kaylaychi77@gmail.com>
2026-04-15 17:05:28 +0100

    update: morpho scanner

--- scanners/core/morpho-scanner.ts
+++ scanners/core/morpho-scanner.ts
@@ -41,10 +41,84 @@ async function getLiquidateEventMainnet(fromBlock: bigint, toBlock: bigint) {
   return mainnetLogs;
 }
 
+async function getLiquidateEventArbitrum(fromBlock: bigint, toBlock: bigint) {
+  const arbitrumLogs = await publicClientArbitrum.getLogs({
+    address: morphoBlueAddresses[42161],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return arbitrumLogs;
+}
+
+async function getLiquidateEventOptimism(fromBlock: bigint, toBlock: bigint) {
+  const optimismLogs = await publicClientOptimism.getLogs({
+    address: morphoBlueAddresses[10],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return optimismLogs;
+}
+
+async function getLiquidateEventAvalanche(fromBlock: bigint, toBlock: bigint) {
+  const avalancheLogs = await publicClientAvalanche.getLogs({
+    address: morphoBlueAddresses[43114],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return avalancheLogs;
+}
+
+async function getLiquidateEventBase(fromBlock: bigint, toBlock: bigint) {
+  const baseLogs = await publicClientBase.getLogs({
+    address: morphoBlueAddresses[8453],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return baseLogs;
+}
+
+async function getLiquidateEventLinea(fromBlock: bigint, toBlock: bigint) {
+  const lineaLogs = await publicClientLinea.getLogs({
+    address: morphoBlueAddresses[59144],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return lineaLogs;
+}
+
+async function getLiquidateEventPolygon(fromBlock: bigint, toBlock: bigint) {
+  const polygonLogs = await publicClientPolygon.getLogs({
+    address: morphoBlueAddresses[137],
+    event: morphoLiquidationEventAbi[0],
+    fromBlock,
+    toBlock,
+  });
+  return polygonLogs;
+}
+
 while (true) {
   const latestBlockMainnetNumber = await latestBlockMainnet();
+  const latestArbitrumBlock = await latestBlockArbitrum();
+  const latestOptimismBlock = await latestBlockOptimism();
+  const latestAvalancheBlock = await latestBlockAvalanche();
+  const latestBaseBlock = await latestBlockBase();
+  const latestLineaBlock = await latestBlockLinea();
+  const latestPolygonBlock = await latestBlockPolygon();
 
-  if (!latestBlockMainnetNumber) {
+  if (
+    !latestBlockMainnetNumber ||
+    !latestArbitrumBlock ||
+    !latestOptimismBlock ||
+    !latestAvalancheBlock ||
+    !latestBaseBlock ||
+    !latestLineaBlock ||
+    !latestPolygonBlock
+  ) {
     await new Promise((r) => setTimeout(r, 30_000));
     continue;
   }
@@ -52,19 +126,101 @@ while (true) {
   const fromBlock = lastIndexedBlock
     ? lastIndexedBlock + 1n
     : latestBlockMainnetNumber - 5n;
+  const fromBlockArbitrum = lastIndexedBlockArbitrum
+    ? lastIndexedBlockArbitrum + 1n
+    : latestArbitrumBlock - 5n;
+  const fromBlockOptimism = lastIndexedBlockOptimism
+    ? lastIndexedBlockOptimism + 1n
+    : latestOptimismBlock - 5n;
+  const fromBlockAvalanche = lastIndexedBlockAvalanche
+    ? lastIndexedBlockAvalanche + 1n
+    : latestAvalancheBlock - 5n;
+  const fromBlockBase = lastIndexedBlockBase
+    ? lastIndexedBlockBase + 1n
+    : latestBaseBlock - 5n;
+  const fromBlockLinea = lastIndexedBlockLinea
+    ? lastIndexedBlockLinea + 1n
+    : latestLineaBlock - 5n;
+  const fromBlockPolygon = lastIndexedBlockPolygon
+    ? lastIndexedBlockPolygon + 1n
+    : latestPolygonBlock - 5n;
 
   const toBlock =
     latestBlockMainnetNumber - fromBlock > 9n
       ? fromBlock + 9n
       : latestBlockMainnetNumber;
+  const toBlockArbitrum =
+    latestArbitrumBlock - fromBlockArbitrum > 9n
+      ? fromBlockArbitrum + 9n
+      : latestArbitrumBlock;
+  const toBlockOptimism =
+    latestOptimismBlock - fromBlockOptimism > 9n
+      ? fromBlockOptimism + 9n
+      : latestOptimismBlock;
+  const toBlockAvalanche =
+    latestAvalancheBlock - fromBlockAvalanche > 9n
+      ? fromBlockAvalanche + 9n
+      : latestAvalancheBlock;
+  const toBlockBase =
+    latestBaseBlock - fromBlockBase > 9n ? fromBlockBase + 9n : latestBaseBlock;
+  const toBlockLinea =
+    latestLineaBlock - fromBlockLinea > 9n
+      ? fromBlockLinea + 9n
+      : latestLineaBlock;
+  const toBlockPolygon =
+    latestPolygonBlock - fromBlockPolygon > 9n
+      ? fromBlockPolygon + 9n
+      : latestPolygonBlock;
 
   const logs = await getLiquidateEventMainnet(fromBlock, toBlock);
+  const arbitrumLogs = await getLiquidateEventArbitrum(
+    fromBlockArbitrum,
+    toBlockArbitrum,
+  );
+  const optimismLogs = await getLiquidateEventOptimism(
+    fromBlockOptimism,
+    toBlockOptimism,
+  );
+  const avalancheLogs = await getLiquidateEventAvalanche(
+    fromBlockAvalanche,
+    toBlockAvalanche,
+  );
+  const baseLogs = await getLiquidateEventBase(fromBlockBase, toBlockBase);
+  const lineaLogs = await getLiquidateEventLinea(fromBlockLinea, toBlockLinea);
+  const polygonLogs = await getLiquidateEventPolygon(
+    fromBlockPolygon,
+    toBlockPolygon,
+  );
 
   console.log(
     `Fetched ${logs.length} logs from block ${fromBlock} to ${latestBlockMainnetNumber}`,
   );
+  console.log(
+    `Fetched ${arbitrumLogs.length} logs from block ${fromBlockArbitrum} to ${latestArbitrumBlock}`,
+  );
+  console.log(
+    `Fetched ${optimismLogs.length} logs from block ${fromBlockOptimism} to ${latestOptimismBlock}`,
+  );
+  console.log(
+    `Fetched ${avalancheLogs.length} logs from block ${fromBlockAvalanche} to ${latestAvalancheBlock}`,
+  );
+  console.log(
+    `Fetched ${baseLogs.length} logs from block ${fromBlockBase} to ${latestBaseBlock}`,
+  );
+  console.log(
+    `Fetched ${lineaLogs.length} logs from block ${fromBlockLinea} to ${latestLineaBlock}`,
+  );
+  console.log(
+    `Fetched ${polygonLogs.length} logs from block ${fromBlockPolygon} to ${latestPolygonBlock}`,
+  );
 
   lastIndexedBlock = toBlock;
+  lastIndexedBlockArbitrum = toBlockArbitrum;
+  lastIndexedBlockOptimism = toBlockOptimism;
+  lastIndexedBlockAvalanche = toBlockAvalanche;
+  lastIndexedBlockBase = toBlockBase;
+  lastIndexedBlockLinea = toBlockLinea;
+  lastIndexedBlockPolygon = toBlockPolygon;
 
   console.log(`Done. Sleeping 30 seconds...`);
   await new Promise((r) => setTimeout(r, 30_000));
