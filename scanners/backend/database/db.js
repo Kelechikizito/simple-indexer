92088d334d68941c1f7b27bdb517ef99ae9f3409
Kelechikizito <kaylaychi77@gmail.com>
2026-04-17 13:03:39 +0100

    feat: postgres database

--- scanners/backend/database/db.js
+++ scanners/backend/database/db.js
@@ -1,5 +1,5 @@
-import postgres from 'postgres'
+import postgres from "postgres";
 
-const sql = postgres('postgres://localhost:5432/simple_indexer')
+const sql = postgres("postgres://localhost:5432/defi_liquidation_indexer");
 
-export default sql
+export default sql;
